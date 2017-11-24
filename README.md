b4650d80cfd3e1a444796ba808afa5abdddb1dbb
Fantop <sethmiller464@yahoo.com>
2017-11-24 13:27:26 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # webScraper-Seth
 Monitor a website for changes.
+
+## How to run
+
+`git clone https://github.com/Fantop/webScraper-Seth.git` or download and extract the zip.
+
+`cd webScraper-Seth`
+
+`pip install BeautifulSoup4`
+
+`python webScraper.py`
+
+Press control-c to exit the program at any time.
+
+
+Example:
+![example](https://i.cubeupload.com/WeFVCp.png)
+
+## Notes
+
+* Make sure you are running Python 2. Use `python --version` to check.
+* Make sure you are giving a unique word for every url you are checking. This is necessary because the program stores copies of the page's html and needs unique file names when it saves these.
+* I did not write the function that parses the visible text from the HTML. Rest of the code is completely original.
---
+++ webScraper.py
@@ -0,0 +1,107 @@
+import os
+import time
+import urllib2
+import shutil
+import sys
+
+try:
+	from bs4 import BeautifulSoup
+	from bs4.element import Comment
+except Exception as e:
+	print "Are you sure you have BeautifulSoup installed? Enter 'pip install BeautifulSoup4' in the terminal to install."
+	sys.exit()
+
+def checkIfUpdated(url="http://example.com/", secondsInterval=5, filePrefix="example", compareText=False):
+    # it's good practice to have headers
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+    req = urllib2.Request(url, headers=hdr)
+
+    # file cleanup
+    if (os.path.isfile(filePrefix + "_old.html")): 
+        os.remove(filePrefix + "_old.html")
+    if (os.path.isfile(filePrefix + "_new.html")): 
+        os.remove(filePrefix + "_new.html")
+    if (not os.path.isfile(filePrefix + "_saved.html")): # check if webpage has been scraped before
+        saved_webpage = open(filePrefix + "_saved.html", "w+")
+        saved_webpage.write(urllib2.urlopen(req).read())
+        saved_webpage.close()
+
+    while True:
+        # load the webpage
+        webpage = urllib2.urlopen(req)
+        webpage_html = webpage.read();
+
+        if (webpage.getcode() == 200): # make sure website returns an OK status code
+            saved_webpage = open(filePrefix + "_saved.html", "r+")
+
+            if (compareText): # compare the visible text of the pages
+                webpageChanged = (text_from_html(saved_webpage.read()).encode('utf-8') != text_from_html(webpage_html).encode('utf-8'))
+            else: # compare the raw html of the pages
+                webpageChanged = (saved_webpage.read() != webpage_html)
+
+            if (webpageChanged):
+                print("Changes detected at " + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) + "!")
+                print("Compare " + filePrefix + "_old.html and " + filePrefix + "_new.html to see differences.")
+    
+                # make a copy of file_saved.html and name it file_old.html
+                shutil.copyfile(filePrefix + "_saved.html", filePrefix + "_old.html")
+                new_webpage = open(filePrefix + "_new.html", "w")
+                new_webpage.write(webpage_html)
+                new_webpage.close()
+
+                # update file_saved.html
+                saved_webpage.seek(0)
+                saved_webpage.write(webpage_html)
+                saved_webpage.close()
+
+                # quit the program
+                break
+            else:
+                print("No changes detected at " + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) + ".")
+
+                # update file_saved.html
+                saved_webpage.seek(0)
+                saved_webpage.write(webpage_html)
+                saved_webpage.close()
+        else:
+            print("Error, could not fetch webpage. Status code: " + str(webpage.getcode()) + ".")
+
+        # wait however many seconds specified
+        time.sleep(secondsInterval)
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)
+    return u" ".join(t.strip() for t in visible_texts)
+
+urlInput = str(raw_input("What url do you want to monitor for updates? "))
+updateInput = int(raw_input("\nHow many seconds do you want to wait between checks (5 seconds minimum)? "))
+
+if (updateInput < 5):
+	print("Wait time too low. Page will be checked every 5 seconds.")
+	updateInput = 5
+
+filePrefixInput = str(raw_input("\nEnter an alphanumeric word (letters and numbers only). You should use a different word for every new website you want to check: "))
+compareTextString = str(raw_input("\nDo you want to use the fancy method to check for updates to the page? (yes/no)\n(I recommend NOT using this method until you've tried the normal method first) "))
+
+if (compareTextString.lower() == "yes" or compareTextString.lower() == "y"):
+	compareTextBoolean = True
+else:
+	compareTextBoolean = False
+
+print("")
+
+checkIfUpdated(urlInput, updateInput, filePrefixInput, compareTextBoolean)
